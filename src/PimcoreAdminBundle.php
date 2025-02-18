a8a5f0a5e8cf5b410e24c7893acf900d2ad9a439
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2025-02-18 10:06:23 +0000

    Apply php-cs-fixer changes

--- src/PimcoreAdminBundle.php
+++ src/PimcoreAdminBundle.php
@@ -31,7 +31,6 @@ use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
 
 class PimcoreAdminBundle extends AbstractPimcoreBundle implements DependentBundleInterface
 {
-
     use PackageVersionTrait;
 
     public function getComposerPackageName(): string
